ce21fbaca14ca25b9292437be25a1aedcca25637
Zohar <juj22@pitt.edu>
2021-09-25 22:52:53 -0400

    main part complete

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,4 +20,11 @@ export class HeroService {
     
     return heroes;
   }
+  getHero(id:number):Observable<Hero>{
+    
+    const hero=HEROES.find(h=>h.id===id)!;
+    this.messageService.add(`HeroService:fetched hero id=${id}`);
+    
+    return of(hero);
+  }
 }
